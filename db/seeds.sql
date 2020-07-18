2df8ba14a93779948a752b13133e8f8524f42eb8
mjaeschke <jaeschke21@gmail.com>
2020-07-17 23:39:37 -0500

    db
    created the initial npm init and installs created the db and some seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name,devoured) VALUES('angus',false);
+INSERT INTO burgers (burger_name,devoured) VALUES('wellington',false);
+INSERT INTO burgers (burger_name,devoured) VALUES('steak',true);
