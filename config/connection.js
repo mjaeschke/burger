c3c7db68a4a353ac268c878c9491bf2772f1bb8e
mjaeschke <jaeschke21@gmail.com>
2020-07-19 00:00:18 -0500

    added stuff for the heroku app

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,21 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "mjaeschke",
-  password: "password1",
-  database: "burgers_db"
-});
+
+
+if (process.env.JAWSDB_URL){
+  connection =mysql.createConnection(process.env.JAWSDB_URL);
+}else{
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "mjaeschke",
+    password: "password1",
+    database: "burgers_db"
+  });
+}
+
+
 //var connection = mysql.createConnection({
   //host: "kf3k4aywsrp0d2is.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   //port: 3306,
